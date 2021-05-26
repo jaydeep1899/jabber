b2885034c0da6728f82eb460ab55b112e3b75407
Sumit Jaiswal <54527500+ransomsumit@users.noreply.github.com>
2021-05-26 09:13:37 +0530

    '1.0.0.4'

--- app.js
+++ app.js
@@ -7,15 +7,24 @@ function query()
     if(y.length > 0)
     {
         y[0].classList.toggle('show');
+        document.getElementById("dis-btn").style.display = 'none';
     }
     document.getElementsByClassName("pin-hid")[0].classList.toggle("show");
+    document.getElementById("pin-btn").style.display = 'inline';
   }
   else if(x == 'District'){
     var y = document.getElementsByClassName("show");
     if(y.length > 0)
     {
         y[0].classList.toggle('show');
+        document.getElementById("pin-btn").style.display = 'none';
     }
     document.getElementsByClassName("dis-hid")[0].classList.toggle("show");
+    document.getElementById("dis-btn").style.display = 'inline';
   }
+}
+
+function getPin()
+{
+
 }
