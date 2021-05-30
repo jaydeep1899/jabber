4e3471c45d08b2dd2bc1fd5221f652ace95da4a2
Sumit Jaiswal <54527500+ransomsumit@users.noreply.github.com>
2021-05-30 09:51:46 +0530

    1.0.0.5

--- app.js
+++ app.js
@@ -23,8 +23,42 @@ function query()
     document.getElementById("dis-btn").style.display = 'inline';
   }
 }
+function souper(object){
+    temp = "";
 
-function getPin()
-{
+}
+function getPin(){
+  var p = document.getElementById("Pincode").value;
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = dd + '-' + mm + '-' + yyyy;
+  var url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${p}&date=${today}`;
 
+  fetch(url)
+    .then(response => response.json())
+    .then(json => {
+      var radr = JSON.stringify(json);
+      var j = JSON.parse(radr);
+      var temp = souper(j);
+      document.getElementById('tags').innerHTML = `<div class="dib">
+<input class="che" type="checkbox" value="">
+  <label class="">
+    Default checkbox
+  </label>
+</div>
+<hr>
+<div class="dib">
+  <input class="che" type="checkbox" value="">
+  <label class="">
+    Checked checkbox
+  </label>
+</div><hr><div class="dib">
+  <input class="che" type="checkbox" value="">
+  <label class="">
+    Checked checkbox
+  </label>
+</div><hr>`;
+    });
 }
